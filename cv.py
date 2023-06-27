fe6debf7451379193bd094d856088be4ac170586
Benjamin Fischbacher <benjamin.fischbacher@student.tugraz.at>
2023-06-27 21:37:39 +0200

    added gaze-tracking

--- cv.py
+++ cv.py
@@ -1,61 +1,68 @@
+from enum import Enum
 import cv2
 import datetime
 import time
 import pyautogui
+from gaze_tracking import GazeTracking
 
+class Direction(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 1
+    
 absolut_X = 1920
 absolut_Y = 1080
 num_faces = 5
 scale_picture_on_screen = 0.5
 speed = 50
+tracking = Direction.HORIZONTAL
 
 saved_faces = []
 offsets = {'X': 0, 'Y': 0}
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+gaze = GazeTracking()
 video_capture = cv2.VideoCapture(0)
+cv2.namedWindow('custom_win', cv2.WINDOW_FREERATIO)
+
 
 while True:
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # We send this frame to GazeTracking to analyze it
+    gaze.refresh(frame)#
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-    )
-    if len(faces):
-        saved_faces.append(faces[0])
-        if saved_faces.__len__() > 5:
-            saved_faces.pop(0)
-        
-        num = saved_faces.__len__()
-        x, y, w, h = 0, 0, 0, 0
-        for face in saved_faces:
-            x += face[0] / num
-            y += face[1] / num
-            w += face[2] / num
-            h += face[3] / num
-        x, y, w, h = [int(i) for i in [x,y,w,h]]
-        
-        height, width, channels = frame.shape
-
-        relX = x / (width - w)
-        relY = y / (height - h)
-        
-        for [relPos, offset] in ([1-relX, 'X'], [relY, 'Y']):
-            if relPos < 0.25 or relPos > 0.75:
-                offsets[offset] += (-0.5 + relPos) * speed            
-
-        relX *= scale_picture_on_screen
-        relY *= scale_picture_on_screen
-
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        pyautogui.moveTo(absolut_X * 2 - relX * absolut_X + offsets['X'], relY * absolut_Y + offsets['Y'])
+    frame = gaze.annotated_frame()#
+    text = ""
+    relative_shift = 0
+    if gaze.is_blinking():
+        text = "Blinking"
+    elif gaze.is_right():
+        relative_shift = speed
+        text = "Looking right"
+    elif gaze.is_left():
+        text = "Looking left"
+        relative_shift = -speed
+    elif gaze.is_center():
+        text = "Looking center"
+    
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+    try: print(text + '\t\t' + str(gaze.eye_left.pupil.x) + '\t\t' + str(gaze.eye_left.pupil.y))
+    except: pass
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
 
-    cv2.imshow("Video", frame)
+    if tracking == Direction.HORIZONTAL:
+        absolut_X += relative_shift
+    elif tracking == Direction.VERTICAL:
+        absolut_Y += relative_shift
+    pyautogui.moveTo(absolut_X, absolut_Y)
+
+    try: cv2.imshow('Video', frame) 
+    except: pass
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
